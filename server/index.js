c6daee36393fee729b56b182e9c6a486377321e0
sharonmbuyi <sharonmbuyi0@gmail.com>
2024-04-03 15:12:09 +0200

    ajout des la page docteur, et de fonctionnalité du backend

--- server/index.js
+++ server/index.js
@@ -1,40 +1,65 @@
-// const express = require("express");
-// const app = express();
-// const http = require("http");
-// const cors = require("cors");
-// const { Server } = require("socket.io");
 
-// app.use(cors());
+
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  })
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("server running on 3001");
+});
+
+
 
 // const server = http.createServer(app);
+// const io = socketIo(server);
 
-// const io = new Server(server, {
-//   cors: {
-//     origin: "http://localhost:5173",
-//     methods: ["GET", "POST"],
-//   },
-// });
 
 // io.on("connection", (socket) => {
-//   console.log(`User Connected: ${socket.id}`);
-
-//   socket.on("join_room", (data) => {
-//     socket.join(data);
-//     console.log(`User with ID: ${socket.id} joined room: ${data}`);
-//   });
+//   console.log("New client connected");
 
 //   socket.on("send_message", (data) => {
-//     socket.to(data.room).emit("receive_message", data);
-//   })
+//     console.log("Received message:", data);
+//     io.emit("receive_message", data);
+//   });
 
 //   socket.on("disconnect", () => {
-//     console.log("User Disconnected", socket.id);
+//     console.log("Client disconnected");
 //   });
 // });
 
-// server.listen(3001, () => {
-//   console.log("server running on 3001");
-// });
+
+
+
+
+
 
 
 
@@ -48,70 +73,77 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-
-
-
 const prisma = new PrismaClient();
 
+// Route pour l'inscription
+app.post('/signup', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // Vérifier si l'utilisateur existe déjà
+    const existingUser = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (existingUser) {
+      return res.status(400).json({ error: 'Cet utilisateur existe déjà' });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await prisma.user.create({
+      data: {
+        email: email,
+        password: hashedPassword,
+      },
+    });
+
+    // Générer un token JWT
+    const token = jwt.sign(
+      { userId: newUser.id, userEmail: newUser.email },
+      'your-secret-key',
+      { expiresIn: '1h' }
+    );
+
+    // Renvoyer une réponse avec le token
+    res.status(201).json({ message: 'Inscription réussie', token });
+  } catch (error) {
+    console.error('Erreur lors de l\'inscription:', error);
+    res.status(500).json({ error: 'Une erreur est survenue lors de l\'inscription' });
+  }
+});
+
+// Route pour la connexion
 app.post('/login', async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    // Vérifier si l'utilisateur existe dans la base de données
-    const user = await prisma.user.findUnique({ 
-      where: { 
-        email:email, 
-        password:password, 
-      } });
-    if (!user) {
-      return res.status(401).json({ error: 'Nom d\'utilisateur ou mot de passe existant' });
+    // Trouver l'utilisateur dans la base de données
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    // Vérifier si l'utilisateur existe et si le mot de passe est correct
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      return res.status(401).json({ error: 'Adresse email ou mot de passe incorrect' });
     }
 
-    // Vérifier si le mot de passe est correct
-    if (!password) {
-      return res.status(401).json({ error: 'Nom d\'utilisateur ou mot de passe incorrect' });
-    }
-
-    // Générer le jeton JWT
+    // Générer un token JWT
     const token = jwt.sign(
-      { 
-        userId: user.id,
-        userEmail: user.email
-      }, 'your-secret-key', { expiresIn: '1h' });
+      { userId: user.id, userEmail: user.email },
+      'your-secret-key',
+      { expiresIn: '1h' }
+    );
 
-    // Renvoyer le jeton JWT
-    res.status(200).json({ error: 'connexion reussie', token: token });
+    // Renvoyer une réponse avec le token
+    res.status(201).json({ message: 'Connexion réussie', token });
   } catch (error) {
     console.error('Erreur de connexion:', error);
     res.status(500).json({ error: 'Une erreur est survenue lors de la connexion' });
   }
 });
 
-
-
-app.post('/signup', async (req, res) => {
-  const { email, password } = req.body;
-
-  try {
-    // Créer l'utilisateur dans la base de données
-    const newUser = await prisma.user.create({
-      data: {
-        email,
-        password
-      },
-    });
-
-    // Renvoyer une réponse avec les données de l'utilisateur créé
-    res.json(newUser);
-  } catch (error) {
-    console.error('Erreur lors de la création du compte:', error);
-    res.status(500).json({ error: 'Une erreur est survenue lors de la création du compte' });
-  }
-});
-
-
-
-
-app.listen(3001, () => {
-  console.log('Serveur à l\'écoute sur le port 3001');
+// Démarrer le serveur
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Serveur en cours d'exécution sur le port ${PORT}`);
 });
